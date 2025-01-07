81afd90bc95c85cbd535456cec56159bd18fdbb2
maxim-peeters <maximpeeters38@gmail.com>
2025-01-07 17:27:11 +0100

    lesson 69

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
+    <FilterNav :current="current" @filterChange="current = $event"/>
     <div v-if="projects.length">
-      <div v-for="project in projects" :key="project.id">
+      <div v-for="project in filteredProjects" :key="project.id">
         <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete"/>
       </div>
     </div>
@@ -10,15 +11,16 @@
 
 <script>
 import SingleProject from '@/components/SingleProject.vue';
-
+import FilterNav from '@/components/FilterNav.vue';
 // @ is an alias to /src
 
 export default {
   name: 'HomeView',
-  components: {SingleProject},
+  components: {SingleProject, FilterNav},
   data(){
     return {
-      projects: []
+      projects: [],
+      current: 'all'
     }
   },
   mounted(){
@@ -35,6 +37,13 @@ export default {
       let p = this.projects.find(project => project.id === id)
       p.complete = !p.complete
     }
+  },
+  computed: {
+    filteredProjects(){
+      if(this.current === 'completed') return this.projects.filter(project => project.complete)
+      if(this.current === 'ongoing') return this.projects.filter(project => !project.complete)
+      return this.projects
+      }
   }
 }
 </script>
