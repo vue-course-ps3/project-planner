c115e80d3da90b535c51c8b6b2ce7c8039c35599
maxim-peeters <maximpeeters38@gmail.com>
2025-01-07 17:01:53 +0100

    lesson 63

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AddProjectView from '@/views/AddProjectView.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'Home',
     component: HomeView
+  },
+  {
+    path: '/add',
+    name: 'AddProject',
+    component: AddProjectView
   },
   
 ]
